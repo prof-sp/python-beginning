c816bc232da5e3c05688ed8a8df3cfc617fe6830
prof-sp <suleimanphares@gmail.com>
2025-08-07 03:00:41 +0300

    Complete list operations for week2 assignment: append, insert, extend, remove, sort, and find index

---
+++ week2/lists.py
@@ -0,0 +1,23 @@
+my_list = [10]
+#print(my_list)
+#Append the following elements to my_list: 10, 20, 30, 40.
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+#print(my_list)
+#Insert the value 15 at the second position in the list.
+my_list.insert(1, 15)
+#print(my_list)
+#Extend my_list with another list: [50, 60, 70].
+my_list.extend([50, 60,70])
+#print(my_list)
+#Remove the last element from my_list.
+#my_list.remove(70)
+del my_list[7]
+#print(my_list)
+#Sort my_list in ascending order.
+my_list.sort()
+#print(my_list)
+#Find and print the index of the value 30 in my_list.
+index_30 = my_list.index(30)
+#print("Index of 30:", index_30)
